b7f3ecd2c800f510152409ee3f6bba53405356fe
w2sv <zangenbergjanek@googlemail.com>
2022-11-21 14:39:02 +0100

    Provide sharedPreferences

--- app/src/main/java/com/w2sv/wifiwidget/preferences/WidgetPreferences.kt
+++ app/src/main/java/com/w2sv/wifiwidget/preferences/WidgetPreferences.kt
@@ -3,8 +3,10 @@ package com.w2sv.wifiwidget.preferences
 import android.content.SharedPreferences
 import com.w2sv.typedpreferences.descendants.BooleanPreferences
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class WidgetPreferences(sharedPreferences: SharedPreferences) :
+@Singleton
+class WidgetPreferences @Inject constructor(sharedPreferences: SharedPreferences) :
     BooleanPreferences(
         "showSSID" to false,
         "showIPv4" to true,
