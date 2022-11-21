aa788920589fed1d1e87ca8f43b23388250c9f8c
w2sv <zangenbergjanek@googlemail.com>
2022-11-21 14:39:02 +0100

    Provide sharedPreferences

--- app/src/main/java/com/w2sv/wifiwidget/preferences/BooleanPreferences.kt
+++ app/src/main/java/com/w2sv/wifiwidget/preferences/BooleanPreferences.kt
@@ -3,8 +3,10 @@ package com.w2sv.wifiwidget.preferences
 import android.content.SharedPreferences
 import com.w2sv.typedpreferences.descendants.BooleanPreferences
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class BooleanPreferences(sharedPreferences: SharedPreferences) :
+@Singleton
+class BooleanPreferences @Inject constructor(sharedPreferences: SharedPreferences) :
     BooleanPreferences(
         "locationPermissionDialogShown" to false,
         sharedPreferences = sharedPreferences
