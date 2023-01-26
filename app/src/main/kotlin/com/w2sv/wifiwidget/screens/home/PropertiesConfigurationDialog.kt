1c111cf134f04300ad771b13b1704f4179d8f2e4
w2sv <zangenbergjanek@googlemail.com>
2023-01-26 21:31:33 +0100

    Migrate to properties configuration dialog; Add WifiConnectionDependentWidgetLayoutSetter

---
+++ app/src/main/kotlin/com/w2sv/wifiwidget/screens/home/PropertiesConfigurationDialog.kt
@@ -0,0 +1,111 @@
+package com.w2sv.wifiwidget.screens.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.w2sv.androidutils.extensions.showToast
+import com.w2sv.wifiwidget.R
+import com.w2sv.wifiwidget.ui.JostText
+
+@Composable
+fun PropertiesConfigurationDialog(onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        ElevatedCard(
+            shape = RoundedCornerShape(12.dp),
+            elevation = CardDefaults.elevatedCardElevation(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                // gradient background
+                modifier = Modifier.background(
+                    Brush.linearGradient(
+                        listOf(
+                            colorResource(id = R.color.mischka_dark),
+                            colorResource(id = R.color.mischka)
+                        ),
+                        start = Offset(0f, Float.POSITIVE_INFINITY),
+                        end = Offset(Float.POSITIVE_INFINITY, 0f)
+                    )
+                )
+            ) {
+                JostText(
+                    text = "Configure shown properties",
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(
+                        top = 24.dp,
+                        bottom = 12.dp,
+                        start = 18.dp,
+                        end = 18.dp
+                    ),
+                    color = colorResource(id = R.color.blue_chill_dark)
+                )
+                Divider(Modifier.padding(horizontal = 22.dp, vertical = 12.dp), color = Color.White)
+                WidgetPropertiesSelectionColumn()
+            }
+        }
+    }
+}
+
+@Composable
+private fun ColumnScope.WidgetPropertiesSelectionColumn(
+) {
+    val viewModel: HomeActivity.ViewModel = viewModel()
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 26.dp)
+            .verticalScroll(rememberScrollState())
+            .weight(1f, fill = false)
+    ) {
+        stringArrayResource(id = R.array.widget_properties)
+            .forEach { widgetProperty ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    JostText(
+                        text = widgetProperty,
+                        modifier = Modifier.weight(1f, fill = true),
+                        color = Color.White,
+                        fontSize = 14.sp
+                    )
+                    Checkbox(
+                        checked = viewModel.widgetPropertyStates.getValue(widgetProperty),
+                        onCheckedChange = {
+                            if (viewModel.unchecksEverything(it, widgetProperty))
+                                context.showToast("You have to leave at least one property checked!")
+                            else
+                                viewModel.widgetPropertyStates[widgetProperty] = it
+                        },
+                        colors = CheckboxDefaults.colors(checkedColor = MaterialTheme.colorScheme.primary)
+                    )
+                }
+            }
+    }
+}
---
+++ app/src/main/kotlin/com/w2sv/wifiwidget/widget/WifiConnectionDependentWidgetLayoutSetter.kt
@@ -0,0 +1,142 @@
+package com.w2sv.wifiwidget.widget
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import android.view.View
+import android.widget.RemoteViews
+import androidx.annotation.IdRes
+import androidx.annotation.StringRes
+import com.w2sv.wifiwidget.R
+import com.w2sv.wifiwidget.preferences.WidgetProperties
+import com.w2sv.wifiwidget.widget.utils.asFormattedIpAddress
+import com.w2sv.wifiwidget.widget.utils.crossVisualize
+import com.w2sv.wifiwidget.widget.utils.isWifiConnected
+import com.w2sv.wifiwidget.widget.utils.netmask
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+
+internal class WifiConnectionDependentWidgetLayoutSetter @Inject constructor() {
+
+    @InstallIn(SingletonComponent::class)
+    @EntryPoint
+    interface EntryPointInterface {
+        fun getInstance(): WifiConnectionDependentWidgetLayoutSetter
+    }
+
+    companion object {
+        fun getInstance(context: Context): WifiConnectionDependentWidgetLayoutSetter =
+            EntryPointAccessors.fromApplication(
+                context,
+                EntryPointInterface::class.java
+            )
+                .getInstance()
+    }
+
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+
+    @Inject
+    lateinit var widgetProperties: WidgetProperties
+
+    /**
+     * connectivityManager.getLinkProperties(connectivityManager.activeNetwork)!! -> {InterfaceName: wlan0 LinkAddresses: [ fe80::ac57:89ff:fe22:9f70/64,192.168.1.233/24,2a02:3036:20a:9df2:ac57:89ff:fe22:9f70/64,2a02:3036:20a:9df2:79d9:9c65:ad9e:81ab/64 ] DnsAddresses: [ /192.168.1.1 ] Domains: null MTU: 1500 ServerAddress: /192.168.1.1 TcpBufferSizes: 1730560,3461120,6922240,524288,1048576,4525824 Routes: [ fe80::/64 -> :: wlan0 mtu 0,::/0 -> fe80::49c8:81bb:cfd2:ce7a wlan0 mtu 0,2a02:3036:20a:9df2::/64 -> :: wlan0 mtu 0,192.168.1.0/24 -> 0.0.0.0 wlan0 mtu 0,0.0.0.0/0 -> 192.168.1.1 wlan0 mtu 0 ]}
+     */
+    fun setOn(remoteViews: RemoteViews) {
+        with(remoteViews) {
+            when (context.getSystemService(WifiManager::class.java).isWifiEnabled) {
+                true -> {
+                    when (context.getSystemService(ConnectivityManager::class.java).isWifiConnected) {
+                        true -> {
+                            populatePropertiesLayout()
+                            crossVisualize(R.id.property_layout, R.id.wifi_status_tv)
+                        }
+
+                        false -> onNoWifiConnectionAvailable(context.getString(R.string.no_wifi_connection))
+                    }
+                }
+
+                false -> onNoWifiConnectionAvailable(context.getString(R.string.wifi_disabled))
+            }
+        }
+    }
+
+    /**
+     * dhcpInfo: 'ipaddr 192.168.1.233 gateway 192.168.1.1 netmask 0.0.0.0 dns1 192.168.1.1 dns2 0.0.0.0 DHCP server 192.168.1.1 lease 28800 seconds'
+     * connectionInfo: 'connection info: SSID: , BSSID: 02:00:00:00:00:00, MAC: 02:00:00:00:00:00, Supplicant state: COMPLETED, Wi-Fi standard: 5, RSSI: -47, Link speed: 433Mbps, Tx Link speed: 433Mbps, Max Supported Tx Link speed: 433Mbps, Rx Link speed: -1Mbps, Max Supported Rx Link speed: 433Mbps, Frequency: 5180MHz, Net ID: -1, Metered hint: false, score: 60'
+     */
+    @Suppress("DEPRECATION")
+    private fun RemoteViews.populatePropertiesLayout() {
+        val wifiManager = context.getSystemService(WifiManager::class.java)
+
+        setPropertyRow(
+            R.id.ssid_row,
+            R.string.ssid,
+            R.id.ssid_tv,
+            R.id.ssid_value_tv
+        ) { wifiManager.connectionInfo.ssid.replace("\"", "") }
+        setPropertyRow(
+            R.id.ip_row,
+            R.string.ip,
+            R.id.ip_tv,
+            R.id.ip_value_tv
+        ) { wifiManager.connectionInfo.ipAddress.asFormattedIpAddress() }
+        setPropertyRow(
+            R.id.frequency_row,
+            R.string.frequency,
+            R.id.frequency_tv,
+            R.id.frequency_value_tv
+        ) { "${wifiManager.connectionInfo.frequency}Hz" }
+        setPropertyRow(
+            R.id.gateway_row,
+            R.string.gateway,
+            R.id.gateway_tv,
+            R.id.gateway_value_tv
+        ) { wifiManager.dhcpInfo.gateway.asFormattedIpAddress() }
+        setPropertyRow(
+            R.id.netmask_row,
+            R.string.netmask,
+            R.id.netmask_tv,
+            R.id.netmask_value_tv
+        ) { netmask() }
+        setPropertyRow(
+            R.id.dns_row,
+            R.string.dns,
+            R.id.dns_tv,
+            R.id.dns_value_tv
+        ) { wifiManager.dhcpInfo.dns1.asFormattedIpAddress() }
+        setPropertyRow(
+            R.id.dhcp_row,
+            R.string.dhcp,
+            R.id.dhcp_tv,
+            R.id.dhcp_value_tv
+        ) { wifiManager.dhcpInfo.serverAddress.asFormattedIpAddress() }
+    }
+
+    private fun RemoteViews.setPropertyRow(
+        @IdRes layout: Int,
+        @StringRes property: Int,
+        @IdRes labelTV: Int,
+        @IdRes valueTV: Int,
+        getValue: () -> String
+    ) {
+        if (widgetProperties.getValue(context.getString(property))) {
+            setTextViewText(labelTV, context.getString(property))
+            setTextViewText(valueTV, getValue())
+
+            setViewVisibility(layout, View.VISIBLE)
+        } else {
+            setViewVisibility(layout, View.GONE)
+        }
+    }
+
+    private fun RemoteViews.onNoWifiConnectionAvailable(statusTvText: String) {
+        setTextViewText(R.id.wifi_status_tv, statusTvText)
+        crossVisualize(R.id.wifi_status_tv, R.id.property_layout)
+    }
+}
