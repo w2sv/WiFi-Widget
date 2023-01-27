0122370d455fe448a6836bdedd18922c94d7d18c
w2sv <zangenbergjanek@googlemail.com>
2023-01-27 19:09:22 +0100

    Structure colors

--- app/src/main/kotlin/com/w2sv/wifiwidget/ui/TopBar.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/TopBar.kt
@@ -1,11 +1,10 @@
 package com.w2sv.wifiwidget.ui
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.w2sv.wifiwidget.R
@@ -17,10 +16,8 @@ fun AppTopBar() {
     TopAppBar(
         { JostText(stringResource(id = R.string.app_name)) },
         colors = TopAppBarDefaults.smallTopAppBarColors(
-            containerColor = colorResource(
-                id = R.color.blue_chill_dark
-            ),
-            titleContentColor = Color.White
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
         )
     )
 }
