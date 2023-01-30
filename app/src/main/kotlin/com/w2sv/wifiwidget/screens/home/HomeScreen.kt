25e054b8021d1746fd3a48e3e9c4b62262251770
w2sv <zangenbergjanek@googlemail.com>
2023-01-30 20:02:10 +0100

    Proper NavigationDrawer

--- app/src/main/kotlin/com/w2sv/wifiwidget/screens/home/HomeScreen.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/screens/home/HomeScreen.kt
@@ -1,15 +1,11 @@
 package com.w2sv.wifiwidget.screens.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,14 +13,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.w2sv.wifiwidget.ui.AppTopBar
 import com.w2sv.wifiwidget.ui.JostText
-import java.util.Calendar
+import kotlinx.coroutines.launch
+import java.util.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 internal fun HomeScreen() {
-    NavigationDrawer {
-        Scaffold(topBar = { AppTopBar() }) { paddingValues ->
+    val coroutineScope = rememberCoroutineScope()
+
+    NavigationDrawer { drawerState ->
+        Scaffold(topBar = {
+            AppTopBar {
+                coroutineScope.launch {
+                    drawerState.open()
+                }
+            }
+        }) { paddingValues ->
             Column(
                 Modifier
                     .padding(paddingValues)
