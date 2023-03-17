f140c086e2016fb7b4108ed0099ecd11131d1657
w2sv <zangenbergjanek@googlemail.com>
2023-03-17 11:05:47 +0100

    Restructure

--- app/src/main/kotlin/com/w2sv/wifiwidget/ui/home/NavigationDrawer.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/home/NavigationDrawer.kt
@@ -40,16 +40,14 @@ import com.w2sv.androidutils.extensions.showToast
 import com.w2sv.wifiwidget.BuildConfig
 import com.w2sv.wifiwidget.R
 import com.w2sv.wifiwidget.activities.HomeActivity
-import com.w2sv.wifiwidget.ui.shared.DialogButton
 import com.w2sv.wifiwidget.ui.shared.JostText
-import com.w2sv.wifiwidget.ui.shared.ThemeSelectionRow
 import com.w2sv.wifiwidget.ui.shared.WifiWidgetTheme
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-private fun NavigationDrawerPrev() {
+private fun Prev() {
     WifiWidgetTheme {
         StatefulNavigationDrawer(initialValue = DrawerValue.Open) {
         }
@@ -108,7 +106,7 @@ fun StatefulNavigationDrawer(
     val context = LocalContext.current
 
     if (showThemeDialog) {
-        ThemeDialog(
+        ThemeSelectionDialog(
             onDismissRequest = {
                 viewModel.inAppThemeState.reset()
                 setShowThemeDialog(false)
@@ -251,52 +249,4 @@ private fun NavigationDrawerItem(
             fontWeight = FontWeight.Medium
         )
     }
-}
-
-@Preview
-@Composable
-fun ThemeDialogPrev() {
-    WifiWidgetTheme {
-        ThemeDialog(
-            onDismissRequest = { /*TODO*/ },
-            theme = { 1 },
-            onThemeSelected = {},
-            applyButtonEnabled = { true },
-            onApplyButtonPress = {}
-        )
-    }
-}
-
-@Composable
-private fun ThemeDialog(
-    onDismissRequest: () -> Unit,
-    theme: () -> Int,
-    onThemeSelected: (Int) -> Unit,
-    applyButtonEnabled: () -> Boolean,
-    onApplyButtonPress: () -> Unit
-) {
-    AlertDialog(
-        onDismissRequest = onDismissRequest,
-        title = { JostText(text = stringResource(id = R.string.theme)) },
-        icon = {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_nightlight_24),
-                contentDescription = "@null",
-                tint = MaterialTheme.colorScheme.primary
-            )
-        },
-        confirmButton = {
-            DialogButton(onClick = { onApplyButtonPress() }, enabled = applyButtonEnabled()) {
-                JostText(text = stringResource(id = R.string.apply))
-            }
-        },
-        dismissButton = {
-            DialogButton(onClick = onDismissRequest) {
-                JostText(text = stringResource(id = R.string.cancel))
-            }
-        },
-        text = {
-            ThemeSelectionRow(selected = theme, onSelected = onThemeSelected)
-        }
-    )
 }
---
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/home/ThemeSelectionDialog.kt
@@ -0,0 +1,62 @@
+package com.w2sv.wifiwidget.ui.home
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.w2sv.wifiwidget.R
+import com.w2sv.wifiwidget.ui.shared.DialogButton
+import com.w2sv.wifiwidget.ui.shared.JostText
+import com.w2sv.wifiwidget.ui.shared.ThemeSelectionRow
+import com.w2sv.wifiwidget.ui.shared.WifiWidgetTheme
+
+@Preview
+@Composable
+private fun Prev() {
+    WifiWidgetTheme {
+        ThemeSelectionDialog(
+            onDismissRequest = { /*TODO*/ },
+            theme = { 1 },
+            onThemeSelected = {},
+            applyButtonEnabled = { true },
+            onApplyButtonPress = {}
+        )
+    }
+}
+
+@Composable
+fun ThemeSelectionDialog(
+    onDismissRequest: () -> Unit,
+    theme: () -> Int,
+    onThemeSelected: (Int) -> Unit,
+    applyButtonEnabled: () -> Boolean,
+    onApplyButtonPress: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = { JostText(text = stringResource(id = R.string.theme)) },
+        icon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_nightlight_24),
+                contentDescription = "@null",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        },
+        confirmButton = {
+            DialogButton(onClick = { onApplyButtonPress() }, enabled = applyButtonEnabled()) {
+                JostText(text = stringResource(id = R.string.apply))
+            }
+        },
+        dismissButton = {
+            DialogButton(onClick = onDismissRequest) {
+                JostText(text = stringResource(id = R.string.cancel))
+            }
+        },
+        text = {
+            ThemeSelectionRow(selected = theme, onSelected = onThemeSelected)
+        }
+    )
+}
