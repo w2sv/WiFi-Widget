1b4514af8f7ba9928e169318dfa360c0dae31779
w2sv <zangenbergjanek@googlemail.com>
2023-04-06 17:27:10 +0200

    Basic integration

--- app/src/main/kotlin/com/w2sv/wifiwidget/ui/screens/home/widgetconfiguration/configcolumn/ColorPickerDialog.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/screens/home/widgetconfiguration/configcolumn/ColorPickerDialog.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
@@ -19,15 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import com.github.skydoves.colorpicker.compose.BrightnessSlider
-import com.github.skydoves.colorpicker.compose.HsvColorPicker
-import com.github.skydoves.colorpicker.compose.rememberColorPickerController
-import com.w2sv.common.extensions.toRGBInt
+import com.smarttoolfactory.colorpicker.picker.ColorPickerCircleValueHSV
 import com.w2sv.wifiwidget.R
 import com.w2sv.wifiwidget.ui.screens.home.HomeActivity
 import com.w2sv.wifiwidget.ui.shared.DialogButton
@@ -43,10 +39,8 @@ internal fun ColorPickerDialog(
     modifier: Modifier = Modifier,
     viewModel: HomeActivity.ViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
-    val controller = rememberColorPickerController()
-
-    var color by remember(properties.hashCode()) {
-        mutableStateOf(controller.selectedColor.value)
+    var color by remember {
+        mutableStateOf(Color(viewModel.customWidgetColorsState[properties.label]!!))
     }
 
     Dialog(onDismissRequest = viewModel::onDismissCustomizationDialog) {
@@ -63,27 +57,9 @@ internal fun ColorPickerDialog(
                     text = properties.label,
                     fontSize = MaterialTheme.typography.headlineMedium.fontSize
                 )
-                HsvColorPicker(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .height(300.dp),
-                    controller = controller,
-                    onColorChanged = {
-                        color = it.color
-                    }
-                )
-                BrightnessSlider(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 10.dp)
-                        .height(36.dp),
-                    controller = controller
-                )
-                Spacer(modifier = Modifier.padding(vertical = 12.dp))
-                JostText(
-                    text = "RGB(${color.red.toRGBInt()}, ${color.green.toRGBInt()}, ${color.blue.toRGBInt()})",
-                    color = color
-                )
+                ColorPickerCircleValueHSV(
+                    initialColor = color,
+                    onColorChange = { newColor, _ -> color = newColor })
                 Spacer(modifier = Modifier.padding(vertical = 12.dp))
                 Row(
                     horizontalArrangement = Arrangement.Center,
@@ -94,8 +70,7 @@ internal fun ColorPickerDialog(
                     }
                     Spacer(modifier = Modifier.padding(horizontal = 12.dp))
                     DialogButton(onClick = {
-                        viewModel.customWidgetColorsState[properties.label] =
-                            controller.selectedColor.value.toArgb()
+                        viewModel.customWidgetColorsState[properties.label] = color.toArgb()
                         viewModel.onDismissCustomizationDialog()
                     }) {
                         JostText(text = stringResource(id = R.string.okay))
