833819039e53e8a4d4834102696f4a6e0a105cf7
w2sv <zangenbergjanek@googlemail.com>
2023-08-08 12:04:14 +0200

    Refactor

--- widget/src/main/kotlin/com/w2sv/widget/model/WifiPropertyLayoutViewData.kt
+++ widget/src/main/kotlin/com/w2sv/widget/model/WifiPropertyLayoutViewData.kt
@@ -1,13 +1,95 @@
 package com.w2sv.widget.model
 
+import android.content.Context
+import android.view.View
+import android.widget.RemoteViews
 import com.w2sv.data.networking.IPAddress
+import com.w2sv.widget.R
+import com.w2sv.widget.utils.setTextView
 
 internal sealed interface WifiPropertyLayoutViewData {
-    open class IPProperties(
+
+    fun inflateView(context: Context, widgetColors: WidgetColors): RemoteViews
+
+    data class WifiProperty(val label: CharSequence, val value: CharSequence) :
+        WifiPropertyLayoutViewData {
+
+        override fun inflateView(context: Context, widgetColors: WidgetColors): RemoteViews =
+            RemoteViews(context.packageName, R.layout.wifi_property)
+                .apply {
+                    setTextView(
+                        viewId = R.id.property_label_tv,
+                        text = label,
+                        color = widgetColors.primary
+                    )
+                    setTextView(
+                        viewId = R.id.property_value_tv,
+                        text = value,
+                        color = widgetColors.secondary
+                    )
+                }
+    }
+
+    data class IPProperties(
         val ipAddress: IPAddress,
         val showPrefixLength: Boolean,
-    ) : WifiPropertyLayoutViewData
+    ) : WifiPropertyLayoutViewData {
 
-    class WifiProperty(val label: CharSequence, val value: CharSequence) :
-        WifiPropertyLayoutViewData
+        override fun inflateView(context: Context, widgetColors: WidgetColors): RemoteViews =
+            RemoteViews(context.packageName, R.layout.ip_properties)
+                .apply {
+                    val propertyTVIterator = listOf(
+                        R.id.ip_property_tv_1,
+                        R.id.ip_property_tv_2,
+                        R.id.ip_property_tv_3,
+                        R.id.ip_property_tv_4,
+                        R.id.ip_property_tv_5
+                    )
+                        .iterator()
+
+                    fun addSubPropertyTV(text: String) {
+                        setTextView(
+                            viewId = propertyTVIterator.next(),
+                            text = text,
+                            color = widgetColors.secondary
+                        )
+                    }
+
+                    fun addSubPropertyTVIfConditionMet(conditional: Boolean, text: String) {
+                        if (conditional) {
+                            addSubPropertyTV(text)
+                        }
+                    }
+
+                    // Prefix length
+                    addSubPropertyTVIfConditionMet(
+                        showPrefixLength, "/${ipAddress.prefixLength}"
+                    )
+
+                    // Local / Public
+                    if (ipAddress.isLocal) {
+                        addSubPropertyTVIfConditionMet(
+                            ipAddress.localAttributes.siteLocal, "SiteLocal"
+                        )
+                        addSubPropertyTVIfConditionMet(
+                            ipAddress.localAttributes.linkLocal, "LinkLocal"
+                        )
+                        addSubPropertyTVIfConditionMet(
+                            !ipAddress.localAttributes.siteLocal && !ipAddress.localAttributes.linkLocal,
+                            "Local"
+                        )
+                    } else {
+                        addSubPropertyTV("Public")
+                    }
+
+                    // Additional properties
+                    addSubPropertyTVIfConditionMet(ipAddress.isLoopback, "Loopback")
+                    addSubPropertyTVIfConditionMet(ipAddress.isMultiCast, "Multicast")
+
+                    // Hide all remaining sub-property views
+                    propertyTVIterator.forEachRemaining {
+                        setViewVisibility(it, View.GONE)
+                    }
+                }
+    }
 }
--- widget/src/main/kotlin/com/w2sv/widget/ui/WifiPropertyViewsFactory.kt
+++ widget/src/main/kotlin/com/w2sv/widget/ui/WifiPropertyViewsFactory.kt
@@ -1,18 +1,15 @@
 package com.w2sv.widget.ui
 
 import android.content.Context
-import android.view.View
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import com.w2sv.androidutils.coroutines.getSynchronousMap
 import com.w2sv.androidutils.coroutines.getValueSynchronously
 import com.w2sv.data.model.WifiProperty
 import com.w2sv.data.storage.WidgetRepository
-import com.w2sv.widget.R
 import com.w2sv.widget.data.appearance
 import com.w2sv.widget.model.WidgetColors
 import com.w2sv.widget.model.WifiPropertyLayoutViewData
-import com.w2sv.widget.utils.setTextView
 import dagger.hilt.android.qualifiers.ApplicationContext
 import slimber.log.i
 import javax.inject.Inject
@@ -93,80 +90,7 @@ class WifiPropertyViewsFactory @Inject constructor(
     override fun getCount(): Int = propertyViewData.size
 
     override fun getViewAt(position: Int): RemoteViews =
-        when (val viewData = propertyViewData[position]) {
-            is WifiPropertyLayoutViewData.WifiProperty -> {
-                RemoteViews(context.packageName, R.layout.wifi_property).apply {
-                    setTextView(
-                        viewId = R.id.property_label_tv,
-                        text = viewData.label,
-                        color = widgetColors.primary
-                    )
-                    setTextView(
-                        viewId = R.id.property_value_tv,
-                        text = viewData.value,
-                        color = widgetColors.secondary
-                    )
-                }
-            }
-
-            is WifiPropertyLayoutViewData.IPProperties -> {
-                RemoteViews(context.packageName, R.layout.ip_properties)
-                    .apply {
-                        val propertyTVIterator = listOf(
-                            R.id.ip_property_tv_1,
-                            R.id.ip_property_tv_2,
-                            R.id.ip_property_tv_3,
-                            R.id.ip_property_tv_4,
-                            R.id.ip_property_tv_5
-                        )
-                            .iterator()
-
-                        fun addSubPropertyTV(text: String) {
-                            setTextView(
-                                viewId = propertyTVIterator.next(),
-                                text = text,
-                                color = widgetColors.secondary
-                            )
-                        }
-
-                        fun addSubPropertyTVIfConditionMet(conditional: Boolean, text: String) {
-                            if (conditional) {
-                                addSubPropertyTV(text)
-                            }
-                        }
-
-                        // Prefix length
-                        addSubPropertyTVIfConditionMet(
-                            viewData.showPrefixLength, "/${viewData.ipAddress.prefixLength}"
-                        )
-
-                        // Local / Public
-                        if (viewData.ipAddress.isLocal) {
-                            addSubPropertyTVIfConditionMet(
-                                viewData.ipAddress.localAttributes.siteLocal, "SiteLocal"
-                            )
-                            addSubPropertyTVIfConditionMet(
-                                viewData.ipAddress.localAttributes.linkLocal, "LinkLocal"
-                            )
-                            addSubPropertyTVIfConditionMet(
-                                !viewData.ipAddress.localAttributes.siteLocal && !viewData.ipAddress.localAttributes.linkLocal,
-                                "Local"
-                            )
-                        } else {
-                            addSubPropertyTV("Public")
-                        }
-
-                        // Additional properties
-                        addSubPropertyTVIfConditionMet(viewData.ipAddress.isLoopback, "Loopback")
-                        addSubPropertyTVIfConditionMet(viewData.ipAddress.isMultiCast, "Multicast")
-
-                        // Hide all remaining sub-property views
-                        propertyTVIterator.forEachRemaining {
-                            setViewVisibility(it, View.GONE)
-                        }
-                    }
-            }
-        }
+        propertyViewData[position].inflateView(context, widgetColors)
 
     override fun getLoadingView(): RemoteViews? = null
 
