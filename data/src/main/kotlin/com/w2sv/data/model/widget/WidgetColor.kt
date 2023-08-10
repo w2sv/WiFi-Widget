540ff0b347adf5761789f2a23af5072ba77fcbe7
w2sv <zangenbergjanek@googlemail.com>
2023-08-10 21:53:29 +0200

    Change default widget colors

--- data/src/main/kotlin/com/w2sv/data/model/widget/WidgetColor.kt
+++ data/src/main/kotlin/com/w2sv/data/model/widget/WidgetColor.kt
@@ -8,8 +8,7 @@ enum class WidgetColor(
     override val preferencesKey: Preferences.Key<Int>,
     override val defaultValue: Int
 ) : DataStoreEntry.UniType<Int> {
-
-    Background(intPreferencesKey("Background"), -9430904),  // Purplish
-    Primary(intPreferencesKey("Labels"), -65536),  // Red
-    Secondary(intPreferencesKey("Other"), -1)  // White
+    Background(intPreferencesKey("Background"), -7859146),
+    Primary(intPreferencesKey("Labels"), -5898336),
+    Secondary(intPreferencesKey("Other"), -1)
 }
