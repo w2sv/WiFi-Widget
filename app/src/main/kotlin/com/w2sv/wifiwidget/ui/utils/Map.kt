7dc22bcd0a4969d19238285a5fce747eaa8e7466
w2sv <zangenbergjanek@googlemail.com>
2024-04-19 09:42:38 +0200

    fromPersistedFlowMapWithSynchronousInitial

--- app/src/main/kotlin/com/w2sv/wifiwidget/ui/utils/Map.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/utils/Map.kt
@@ -1,26 +1,23 @@
 package com.w2sv.wifiwidget.ui.utils
 
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.snapshots.SnapshotStateMap
 import com.w2sv.androidutils.datastorage.preferences_datastore.flow.DataStoreFlowMap
 import com.w2sv.androidutils.ui.reversible_state.ReversibleStateMap
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 
 fun <K, V> ReversibleStateMap.Companion.fromDataStoreFlowMap(
     dataStoreFlowMap: DataStoreFlowMap<K, V>,
     scope: CoroutineScope,
-    started: SharingStarted = SharingStarted.Eagerly,
     onStateSynced: suspend (Map<K, V>) -> Unit = {}
 ): ReversibleStateMap<K, V> =
-    ReversibleStateMap(
-        persistedStateFlowMap = dataStoreFlowMap.stateIn(scope, started),
-        makeMap = { it.getMutableStateMap() },
+    fromPersistedFlowMapWithSynchronousInitial(
+        persistedFlowMap = dataStoreFlowMap,
+        scope = scope,
+        makeMap = {
+            mutableStateMapOf<K, V>().apply {
+                putAll(it)
+            }
+        },
         syncState = dataStoreFlowMap::save,
         onStateSynced = onStateSynced
     )
-
-private fun <K, V> Map<K, StateFlow<V>>.getMutableStateMap(): SnapshotStateMap<K, V> =
-    mutableStateMapOf<K, V>()
-        .apply { this@getMutableStateMap.forEach { (k, v) -> put(k, v.value) } }
