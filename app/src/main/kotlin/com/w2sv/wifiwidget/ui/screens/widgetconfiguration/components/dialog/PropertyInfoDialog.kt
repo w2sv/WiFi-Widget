853206d9d2f08bfdc108ab7a249565e9b5388958
w2sv <zangenbergjanek@googlemail.com>
2024-11-02 22:48:33 +0100

    Learn more hyperlink text

--- app/src/main/kotlin/com/w2sv/wifiwidget/ui/screens/widgetconfiguration/components/dialog/PropertyInfoDialog.kt
+++ app/src/main/kotlin/com/w2sv/wifiwidget/ui/screens/widgetconfiguration/components/dialog/PropertyInfoDialog.kt
@@ -1,6 +1,5 @@
 package com.w2sv.wifiwidget.ui.screens.widgetconfiguration.components.dialog
 
-import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,18 +8,18 @@ import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
-import com.w2sv.androidutils.openUrl
 import com.w2sv.wifiwidget.R
 import com.w2sv.wifiwidget.ui.designsystem.DialogButton
 import com.w2sv.wifiwidget.ui.designsystem.InfoIcon
@@ -32,26 +31,22 @@ fun PropertyInfoDialog(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val context: Context = LocalContext.current
     InfoDialog(
         modifier = modifier,
         title = data.title,
         text = data.description,
-        learnMoreButton = data.learnMoreUrl?.let {
+        learnMoreButton = data.learnMoreUrl?.let { learnMoreUrl ->
             {
-                ElevatedButton(
-                    onClick = {
-                        context.openUrl(it)
+                Text(
+                    text = buildAnnotatedString {
+                        withLink(LinkAnnotation.Url(learnMoreUrl)) {
+                            append(stringResource(R.string.learn_more))
+                        }
                     },
-                    modifier = Modifier.padding(top = 20.dp, bottom = 12.dp),
-                    colors = ButtonDefaults.elevatedButtonColors(containerColor = MaterialTheme.colorScheme.secondary),
-                    elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 8.dp)
-                ) {
-                    Text(
-                        text = stringResource(R.string.learn_more),
-                        color = MaterialTheme.colorScheme.onSecondary
-                    )
-                }
+                    textDecoration = TextDecoration.Underline,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    modifier = Modifier.padding(top = 20.dp, bottom = 12.dp)
+                )
             }
         },
         onDismissRequest = onDismissRequest,
