099c139cd5dcd1b215a5613da975692f3440cc8a
w2sv <zangenbergjanek@googlemail.com>
2024-11-24 12:26:01 +0100

    Fix IPAddressTest.kt

--- core/networking/src/test/kotlin/com/w2sv/networking/model/IPAddressTest.kt
+++ core/networking/src/test/kotlin/com/w2sv/networking/model/IPAddressTest.kt
@@ -3,6 +3,7 @@ package com.w2sv.networking.model
 import kotlinx.coroutines.test.runTest
 import okhttp3.OkHttpClient
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class IPAddressTest {
 
@@ -10,8 +11,13 @@ class IPAddressTest {
     fun `test fetchPublic`() {
         val httpClient = OkHttpClient()
         runTest {
-            IPAddress.fetchPublic(httpClient, IPAddress.Version.V4).getOrThrow()
-            IPAddress.fetchPublic(httpClient, IPAddress.Version.V6).getOrThrow()
+            val v4FetchResult = IPAddress.fetchPublic(httpClient, IPAddress.Version.V4)
+            val v6FetchResult = IPAddress.fetchPublic(httpClient, IPAddress.Version.V6)
+
+            assertTrue(
+                v4FetchResult.isSuccess || v6FetchResult.isSuccess,
+                "Fetching of both IP versions failed with ${v4FetchResult.exceptionOrNull()} and ${v6FetchResult.exceptionOrNull()}"
+            )
         }
     }
 }
