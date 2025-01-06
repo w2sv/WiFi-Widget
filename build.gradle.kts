fc19d2692b190b1f794d0e075292fa4dbd52b6f5
w2sv <zangenbergjanek@googlemail.com>
2025-01-06 14:01:43 +0100

    Add version-catalog-update-plugin | Update compose-destinations to 2.0.0-beta12, reorderable to 2.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -11,4 +13,25 @@ plugins {
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.baselineprofile) apply false
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.versions)
+    alias(libs.plugins.versionCatalogUpdate)
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
+
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+// Taken from https://github.com/ben-manes/gradle-versions-plugin#rejectversionsif-and-componentselection
+private fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
