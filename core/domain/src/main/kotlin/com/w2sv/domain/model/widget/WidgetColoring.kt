4fb317efda041a91b8e272ad3644424124f81137
w2sv <zangenbergjanek@googlemail.com>
2026-03-09 12:33:25 +0100

    refactor: move default coloring strategies into WidgetColoring

--- core/domain/src/main/kotlin/com/w2sv/domain/model/widget/WidgetColoring.kt
+++ core/domain/src/main/kotlin/com/w2sv/domain/model/widget/WidgetColoring.kt
@@ -1,8 +1,23 @@
 package com.w2sv.domain.model.widget
 
+import com.w2sv.androidutils.os.dynamicColorsSupported
+import com.w2sv.domain.model.Theme
+
+private val defaultPresetColoring = WidgetColoringStrategy.Preset(
+    theme = Theme.Default,
+    useDynamicColors = dynamicColorsSupported
+)
+private val defaultCustomColoring = WidgetColoringStrategy.Custom(
+    WidgetColors(
+        background = -7859146,
+        primary = -5898336,
+        secondary = -1
+    )
+)
+
 data class WidgetColoring(
-    val preset: WidgetColoringStrategy.Preset = WidgetColoringStrategy.Preset(),
-    val custom: WidgetColoringStrategy.Custom = WidgetColoringStrategy.Custom(),
+    val preset: WidgetColoringStrategy.Preset = defaultPresetColoring,
+    val custom: WidgetColoringStrategy.Custom = defaultCustomColoring,
     val useCustom: Boolean = false
 ) {
     val strategies: List<WidgetColoringStrategy>
--- core/domain/src/main/kotlin/com/w2sv/domain/model/widget/WidgetColoringStrategy.kt
+++ core/domain/src/main/kotlin/com/w2sv/domain/model/widget/WidgetColoringStrategy.kt
@@ -1,16 +1,13 @@
 package com.w2sv.domain.model.widget
 
-import com.w2sv.androidutils.os.dynamicColorsSupported
 import com.w2sv.domain.model.Theme
-
-private val defaultCustomColors = WidgetColors(-7859146, -5898336, -1)
 
 sealed interface WidgetColoringStrategy {
 
     val isCustom get() = this is Custom
 
-    data class Preset(val theme: Theme = Theme.Default, val useDynamicColors: Boolean = dynamicColorsSupported) : WidgetColoringStrategy
+    data class Preset(val theme: Theme, val useDynamicColors: Boolean) : WidgetColoringStrategy
 
     @JvmInline
-    value class Custom(val colors: WidgetColors = defaultCustomColors) : WidgetColoringStrategy
+    value class Custom(val colors: WidgetColors) : WidgetColoringStrategy
 }
