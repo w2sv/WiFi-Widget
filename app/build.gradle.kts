6e4b2dc929ae7e15641d54e454dfdfa4b0fafda0
w2sv <zangenbergjanek@googlemail.com>
2026-08-16 11:52:33 +0200

    chore: rename dependency alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,7 +121,7 @@ dependencies {
 
     // Compose libraries
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
